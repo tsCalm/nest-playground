be72c81906e899e49ccaa76bb793d0b6d38d7b34
tsCalm <dlatkdals93@gmail.com>
2022-12-14 17:06:31 +0900

    update: customer module -> data transfer

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,19 +1,23 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
   Req,
   Res,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CreateCustomerDto } from 'src/customers/dtos/createCustomer.dto';
 import { CustomersService } from '../../services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private cusService: CustomersService) {}
+
   @Get(':id')
   getCustomer(
     @Param('id', ParseIntPipe) id: number,
@@ -24,11 +28,22 @@ export class CustomersController {
     if (!customer) res.status(400).send({ msg: 'Customer not found' });
     res.send(customer);
   }
+
   @Get('search/:id')
   searchCustomer(@Param('id', ParseIntPipe) id: number) {
     const customer = this.cusService.findCustomerById(id);
     if (!customer)
       throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
     return customer;
+  }
+
+  @Get('')
+  getCustomers() {
+    return this.cusService.getCustomers();
+  }
+
+  @Post('create')
+  createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+    this.cusService.createCustomer(createCustomerDto);
   }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -1,30 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from '../../types/customer';
+import { CreateCustomerDto } from '../../dtos/createCustomer.dto';
 
 @Injectable()
 export class CustomersService {
-  users = [
+  private customers: Customer[] = [
     {
       id: 1,
       email: 'test1@terst.com',
-      createdAt: new Date(),
+      name: 'testName1',
     },
     {
       id: 2,
       email: 'test2@terst.com',
-      createdAt: new Date(),
+      name: 'testName2',
     },
     {
       id: 3,
       email: 'test3@terst.com',
-      createdAt: new Date(),
+      name: 'testName3',
     },
     {
       id: 4,
       email: 'test4@terst.com',
-      createdAt: new Date(),
+      name: 'testName4',
     },
   ];
   findCustomerById(id: number) {
-    return this.users.find((user) => user.id === id);
+    return this.customers.find((user) => user.id === id);
+  }
+
+  createCustomer(createCustomerDto: CreateCustomerDto) {
+    this.customers.push(createCustomerDto);
+  }
+
+  getCustomers() {
+    return this.customers;
   }
 }
