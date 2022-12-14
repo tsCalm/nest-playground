1908409107c6db6fdeddc5fe357955f64d2997f3
tsCalm <dlatkdals93@gmail.com>
2022-12-14 16:49:36 +0900

    update: customer module -> controller(express, nest diff) & service

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { CustomersService } from '../../services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private cusService: CustomersService) {}
-  @Get()
-  getCustomer() {
-    return this.cusService.findCustomer();
+  @Get(':id')
+  getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const customer = this.cusService.findCustomerById(id);
+    if (!customer) res.status(400).send({ msg: 'Customer not found' });
+    res.send(customer);
+  }
+  @Get('search/:id')
+  searchCustomer(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.cusService.findCustomerById(id);
+    if (!customer)
+      throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
+    return customer;
   }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,29 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  users = [
+    {
       id: 1,
-      email: 'test@terst.com',
+      email: 'test1@terst.com',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: 'test2@terst.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'test3@terst.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 4,
+      email: 'test4@terst.com',
+      createdAt: new Date(),
+    },
+  ];
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
