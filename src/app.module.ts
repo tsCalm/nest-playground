fe9867ad2084fb66685b4775c6612bf3d5df8b6c
tsCalm <dlatkdals93@gmail.com>
2022-12-15 17:31:37 +0900

    feat: typeorm , mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import entities from './typeorm';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest_playground',
+      entities: entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
